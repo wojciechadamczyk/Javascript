c3353e7b96347e018e99e5e91a6939c8d291db23
CodersLab <jacek.tchorzewski@coderslab.pl>
2016-04-07 17:02:26 +0200

    Fixed errors

--- 1.Podstawy_JavaScript/1.Podstawy/js/cwiczenie3.js
+++ 1.Podstawy_JavaScript/1.Podstawy/js/cwiczenie3.js
@@ -16,6 +16,6 @@ function myEval(a, b, operation){
     return result;
 }
 
-console.log(" 2 + 4 = ". myEval(2, 4, "+"));
-console.log(" 5 - 2 = ". myEval(5, 2, "-"));
-console.log(" 4 ^ 8 = ". myEval(4, 8, "^"));
+console.log(" 2 + 4 = " + myEval(2, 4, "+"));
+console.log(" 5 - 2 = " + myEval(5, 2, "-"));
+console.log(" 4 ^ 8 = " + myEval(4, 8, "^"));
